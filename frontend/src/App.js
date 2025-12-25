63e8e77ba3187f7ac5b43b785728cefff4cd32c5
Thelli Govinda Rao <147299435+govindthelli@users.noreply.github.com>
2025-12-25 13:57:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,66 @@
-import React, { Component} from "react";
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 import { hot } from "react-hot-loader";
 import "./App.css";
 
-class App extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            logedin: false,
-            token: ''
-        };
-        this.fetchUser = this.fetchUser.bind(this)
-    }
-    fetchUser (){
-        if(this.state.logedin){
-            this.setState({
-                logedin: false
-            })
-        } else {
-              try {
-               const data = axios.post(
-                    '/users/',
-                    JSON.stringify({
-                        'email': 'testtwo@test.com',
-                        'password': 'testpassword'
-                    }),
-                    {
-                            'Access-Control-Allow-Origin': '*'
-                        }
-                    
-                  ).then(response =>{
-                      console.log(response)
-                      this.setState({
-                          logedin: true,
-                          token: response.data.token
-                      })
-                  })
-              } catch (err) {
-                return err
-              }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logedin: false,
+      data: null
+    };
+    this.fetchUser = this.fetchUser.bind(this);
+  }
+
+  fetchUser() {
+    if (this.state.logedin) {
+      this.setState({ logedin: false, data: null });
+    } else {
+      axios.post(
+        "/users/",                                   // <-- API endpoint
+        {
+          email: "testtwo@test.com",                 // <-- JSON body (no stringify)
+          password: "testpassword"
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"       // <-- correct header
+          }
         }
+      )
+      .then(response => {
+        console.log("Login Success:", response.data);
+        this.setState({
+          logedin: true,
+          data: response.data.data || response.data  // <-- show response data
+        });
+      })
+      .catch(error => {
+        console.log("Login Error:", error.response?.data || error);
+        alert("Authentication Failed");
+      });
     }
-  render(){
-    return(
+  }
+
+  render() {
+    const { logedin, data } = this.state;
+
+    return (
       <div className="App">
-        { this.state.logedin ? (
-            <div className="unlogged">
-              <h1>{this.state.token}</h1>
-               <button className="button" onClick={(e)=> this.fetchUser(e)}>clear</button>
-            </div>
+        {logedin ? (
+          <div className="logged">
+            <h1>Logged In</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <button className="button" onClick={this.fetchUser}>Logout</button>
+          </div>
         ) : (
-            <div className="unlogged">
-                <h1> testtwo@test.com</h1>
-                <h1>testpassword</h1>
-                <button className="button" onClick={(e)=> this.fetchUser(e)}>Send database</button>
-            </div>
-        )
-        }
+          <div className="unlogged">
+            <h1>Email: testtwo@test.com</h1>
+            <h1>Password: testpassword</h1>
+            <button className="button" onClick={this.fetchUser}>Login</button>
+          </div>
+        )}
       </div>
     );
   }
